1efba4f0b87db7c675f16ad622e7c38eddfaed5d
Bekmirza <madaminovbekmirza@gmail.com>
2026-07-24 19:21:15 +0500

    Delete random_example.py

--- random_example.py
+++
@@ -1,9 +0,0 @@
-import random
-m = int(input("mga qiymat kiriting: "))
-n = int(input("nga qiymat kiriting: "))
-if m > n:  
-    print("m katta n dan")
-    print("qiymatni boshqa kiriting!!!")
-    #1break
-a = random.randint(m, n)
-print("Sirli son: ", a)
